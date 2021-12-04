6a555181e1172c08d3b2a258ca49244be28b5990
RamiIsk <ramysaleh60@gmail.com>
2021-12-03 20:55:10 -0800

    second timer push

--- Backend/The6Bits.BitOHealth/The6Bits.BitOHealth.ControllerLayer/Class1.cs
+++ Backend/The6Bits.BitOHealth/The6Bits.BitOHealth.ControllerLayer/Class1.cs
@@ -8,5 +8,18 @@ namespace The6Bits.BitOHealth.ControllerLayer
 {
     class Class1
     {
+        void CheckTime()
+        {
+            var t = new Timer(TimerCallback);
+            DateTime utctime = DateTime.Now;
+            DateTime nine = DateTime.Today.AddHours(21.0); 
+
+            int ms = (int)((fourOClock - now).TotalMilliseconds);
+            if (utctime == nine)
+            {
+
+
+            }
+        }
     }
 }
