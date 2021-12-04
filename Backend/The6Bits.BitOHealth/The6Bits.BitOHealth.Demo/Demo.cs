44b989ca8db478c6f272ecf32eb3486586e0d0ad
RamiIsk <ramysaleh60@gmail.com>
2021-12-04 01:43:31 -0800

    Update Demo.cs

--- Backend/The6Bits.BitOHealth/The6Bits.BitOHealth.Demo/Demo.cs
+++ Backend/The6Bits.BitOHealth/The6Bits.BitOHealth.Demo/Demo.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using The6Bits.BitOHealth.ControllerLayer;
+using The6Bits.BitOHealth.Models;
 namespace The6Bits.BitOHealth.Demo
 {
-    internal class Class1
+    class Demo
     {
+        static void Main(string[] args)
+        {
+            UMController um=new UMController();
+            User rami = new User("Rami", "rami.iskender@student.csulb.edu", "123Rr567","Rami", "Iskender",1,1);
+            String RamiCreate=um.CreateAccount(rami);
+            Console.WriteLine(RamiCreate);
+            String RamiDisable = um.DisableAccount("Rami");
+            Console.WriteLine(RamiDisable);
+            String RamiEnable = um.EnableAccount("Rami");
+            Console.WriteLine(RamiEnable);
+            User rami2= new User("Rami", "rami@gmail.com", "123Rr567", "Ramy", "Saleh", 1, 1);
+            String RamiCreate2 = um.CreateAccount(rami2);
+            Console.WriteLine(RamiCreate2);
+            String RamiDelete=um.DeleteAccount("Rami");
+            Console.WriteLine(RamiDelete);
+            
+
+        }
     }
 }
