f7fd8a36f3f9470fcd8bf521cc85739af87faa10
RamiIsk <ramysaleh60@gmail.com>
2021-12-18 06:55:03 -0800

    umservicetest

---
+++ Backend/The6Bits.BitOHealth/The6Bits.ControllerLayer.Test/UMControllerTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace The6Bits.ControllerLayer.Test
+{
+    public class UMControllerTest
+    {
+        [Fact]
+        public void CreateUser()
+        {
+
+        }
+    }
+}
