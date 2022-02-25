7f66b9f6bbadf4e2b475ef5852820b3d8709e0ef
RamiIsk <ramysaleh60@gmail.com>
2022-02-25 00:41:56 -0800

    Create MsSqlDerrorService.cs

---
+++ Backend/The6Bits.BitOHealth/The6Bits.BitOHealth.ServiceLayer/MsSqlDerrorService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The6Bits.BitOHealth.ServiceLayer
+{
+    public class MsSqlDerrorService
+    {
+        public string DBErrorCheck(int ErrorNumber)
+        {
+            if (ErrorNumber == -2)
+            {
+                return "Database Time Out Error";
+            }
+            else if (ErrorNumber == 1105)
+            {
+                return "Database Full";
+            }
+            else if(ErrorNumber == 4060)
+            {
+                return "Database Offline";
+            }
+            else
+            {
+                return "Database Other Error";
+            }
+        }
+    }
+}
