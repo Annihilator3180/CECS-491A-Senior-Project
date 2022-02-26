a2f26df8aa1c7a1f7078c0c058fe5e6a791ef17b
emilysahyoun <emilysahyoun29@gmail.com>
2022-02-26 05:34:39 -0800

    Update AuthenticationServiceTest.cs

--- Backend/The6Bits.BitOHealth/Tests/The6Bits.BitOHealth.ServiceLayer.Test/AuthenticationServiceTest.cs
+++ Backend/The6Bits.BitOHealth/Tests/The6Bits.BitOHealth.ServiceLayer.Test/AuthenticationServiceTest.cs
@@ -15,7 +15,7 @@ namespace The6Bits.BitOHealth.ServiceLayer.Tests
         }
 
         [Theory]
-        [InlineData("test.sss.ddd")]
+        [InlineData("test.test.test")]
         public void InvalidToken(string token)
         {
             var isValid = _authService.ValidateToken(token);
@@ -36,20 +36,20 @@ namespace The6Bits.BitOHealth.ServiceLayer.Tests
         [InlineData("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImZpcnN0dXNlcjI5IiwiaWF0IjoiMTY0NTg2ODc4NCJ9.D6CaUg_WOE4UxSPljdxb90B3bvCnm9u8IFL9dVM-viw")]
         public void GetUserNameShould(string token)
         {
-            var hardCodedUserName = "firstuser29";
+            var user = "firstuser29";
             var userName = _authService.getUsername(token);
 
-            Assert.Equal(userName, hardCodedUserName);
+            Assert.Equal(userName, user);
         }
 
         [Theory]
         [InlineData("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImZpcnN0dXNlcjI5IiwiaWF0IjoiMTY0NTg2ODc4NCJ9.D6CaUg_WOE4UxSPljdxb90B3bvCnm9u8IFL9dVM-viw")]
         public void GetUserNameShouldNot(string token)
         {
-            var hardCodedUserName = "wronguser";
+            var user = "ddfscsa29";
             var userName = _authService.getUsername(token);
 
-            Assert.NotEqual(userName, hardCodedUserName);
+            Assert.NotEqual(userName, user);
         }
     }
 }
