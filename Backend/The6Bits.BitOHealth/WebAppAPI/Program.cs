407aa575bbb4c88152f56280939b7163853161b1
sebastian <44591891+cbxss@users.noreply.github.com>
2022-03-07 22:43:32 -0800

    vue added

--- Backend/The6Bits.BitOHealth/WebAppAPI/Program.cs
+++ Backend/The6Bits.BitOHealth/WebAppAPI/Program.cs
@@ -66,6 +66,10 @@ if (app.Environment.IsDevelopment())
     //app.UseSwaggerUI();
 }
 
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
 
 
 app.UseHttpsRedirection();
