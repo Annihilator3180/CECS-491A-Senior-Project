dfbbd24ce27dfed34c2e3f57565cd9a541b522ab
Annihilator3180 <munozjacob48@yahoo.com>
2022-03-08 14:38:36 -0800

    Working on Create Records
    Added
    RecordsController
    RecordsManager
    RecordsService

---
+++ Backend/The6Bits.BitOHealth/The6Bits.BitOHealth.ControllerLayer/RecordsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using The6Bits.BitOHealth.DAL;
+using The6Bits.BitOHealth.ManagerLayer;
+using The6Bits.BitOHealth.Models;
+using The6Bits.Logging;
+using The6Bits.Logging.DAL.Implementations;
+using The6Bits.Logging.Implementations;
+using The6Bits.Logging.DAL.Contracts;
+using The6Bits.Authorization.Contract;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.Diagnostics;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using The6Bits.Authentication.Contract;
+using The6Bits.BitOHealth.DAL.Contract;
+namespace The6Bits.BitOHealth.ControllerLayer
+{
+    public class RecordsController : ControllerBase
+    {
+        private RecordsManager _MHM;
+        private LogService logService;
+        private string Username;
+        private bool isValid;
+        private IAuthenticationService _authentication;
+    }
+
+    public class RecordsController(IRepositoryUM<User> daoType, IAuthenticationService authentication, ILogDal logDao)
+    {
+        _MHM = new RecordsManager(daotype);
+        _authentication = authentication;
+        logService = new LogService(logDao); 
+        
+    }
+
+    [HttpPost("CreateRecords")]
+    
+    public string CreateRecords(User u)
+    {
+        isValid = _authentication.ValidateToken(Request.Headers["Authorization"]);
+        string userName = _authenication.getUsername(Request.Headers["Authorizatoin"]);
+
+        if (isValid == null) 
+        { 
+            
+
+
+            
+        }
+    }
+
+
+}
---
+++ Backend/The6Bits.BitOHealth/The6Bits.BitOHealth.ManagerLayer/RecordsManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.Design;
+using The6Bits.BitOHealth.DAL;
+using The6Bits.BitOHealth.DAL.Implementations;
+using The6Bits.BitOHealth.ServiceLayer;
+using The6Bits.BitOHealth.Models;
+using The6Bits.Logging;
+using The6Bits.Logging.Implementations;
+using The6Bits.Authorization.Contract;
+
+namespace The6Bits.BitOHealth.ManagerLayer
+{
+    public class RecordsManager
+    {
+        public IAuthorizationService auth;
+        public string token;
+        private RecordsService _MHS;
+    }
+
+    public MHManager(IRepositoryUM<User> daoType)
+    {
+        _MHS = new RecordsService(daoType);
+    }
+
+    public string ValidateFileSizeRe3cords(User user)
+    {
+        string fileName;
+        string username;
+        int fileSize;
+
+        if(fileSize < 0.5 MB)
+        {
+            return fileName + "File size too small. Try again";
+        }
+        if(fileSize > 12 MB){
+            return fileName + "File size too large. Try again";
+        }
+        
+    }
+}
