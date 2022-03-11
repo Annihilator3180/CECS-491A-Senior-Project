3b474b6fe0e1b125e79d5e9fa9c030d667334715
angelcueva <48697685+angelcueva@users.noreply.github.com>
2022-03-10 23:15:19 -0800

    frontend account recovery updated

--- frontend/the6bits/src/components/AccountRecovery.vue
+++ frontend/the6bits/src/components/AccountRecovery.vue
@@ -1,21 +1,48 @@
 <template>
-  <div class="hello">
+  <div class="form">
     <h1>
       Account Recovery
     </h1>
     <form method = "get" id = "form1">
       <label for = "Email"> Email Address: </label><br>
-      <input type = "text" id = "Email" name = "Email" placeholder="Enter Email Address"> <br>
+      <input type = "text" id = "Email" v-model="formData.email" placeholder="Enter Email Address"> <br>
 
       <label for = "Username"> Username: </label><br>
-      <input type = "text" id = "Username" name = "Username" placeholder="Enter Username"><br>
+      <input type = "text" id = "Username" v-model="formData.username" placeholder="Enter Username"><br>
       </form>
 
-      <input type="submit" value="Send Request">
-
-
+      <button @click = "AccountRecovery"> Recover Account </button>
   </div>
 </template>
+
+<script>
+  export default{
+    name : 'AccountRecovery',
+    data(){
+    return {
+      formData : {
+        email: '',
+        username: '',
+      },
+    }
+  },
+  methods:{
+    AccountRecovery(){
+      const requestOptions = {
+        method: "POST",
+        credentials : 'include',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(
+        { username : this.formData.username,
+          email : this.formData.email
+          })
+      };
+      fetch('https://localhost:7011/Account/Recovery',requestOptions)
+                .then(response => console.log(response))
+    }
+  }
+}
+</script>
 
 
 
