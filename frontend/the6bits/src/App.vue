b581ff78d3c2149bf5c139282d8accd6a50b7d70
sebastian <44591891+cbxss@users.noreply.github.com>
2022-03-12 01:18:09 -0800

    hashfix

--- frontend/the6bits/src/App.vue
+++ frontend/the6bits/src/App.vue
@@ -4,17 +4,13 @@
     <router-link :to="{ name: 'login' }">login</router-link> |
     <router-link :to="{ name: 'otp' }">otp</router-link> |
     <router-link :to="{ name: 'UM' }">User Management</router-link> |
-    <router-link :to="{ name: 'WeightManagement' }"
-      >Weight Management</router-link
-    >
-    |
-    <router-link :to="{ name: 'ResetPassword' }">ResetPassword</router-link>
+    <router-link :to="{ name: 'WeightManagement' }">Weight Management</router-link>|
+    <router-link :to="{ name: 'ResetPassword' }">ResetPassword</router-link>|
+    <router-link :to="{ name: 'AccountRecovery' }">Account Recovery</router-link>|
+    <router-link :to="{ name: 'Registration' }"> Registration</router-link> |
+    <router-link :to="{ name: 'MedSearch' }"> MedSearch</router-link>|
+    <router-link :to="{ name: 'HotTopics' }"> HotTopics</router-link>
 
-    <router-link :to="{ name: 'AccountRecovery' }"
-      >Account Recovery</router-link
-    >
-    | <router-link :to="{ name: 'Registration' }"> Registration</router-link> |
-    <router-link :to="{ name: 'MedSearch' }"> MedSearch</router-link>
   </nav>
   <router-view />
 </template>
