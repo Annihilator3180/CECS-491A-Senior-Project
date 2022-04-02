8205279161b5767473b0a92ea76de09d7a623adf
Annihilator3180 <munozjacob48@yahoo.com>
2022-04-01 23:58:02 -0700

    Modifiying Code

---
+++ Backend/The6Bits.BitOHealth/MapAPI/Implementations/GoogleMapsAPIService.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using MapAPI.Contracts;
+using The6Bits.BitOHealth.Models;
+
+
+namespace MapAPI
+{
+    public class GoogleMapsAPIService<T> : IMapAPI<Parsed>
+    {
+        private readonly HttpClient _client;
+        private readonly string _appId;
+        private readonly string _appKey;
+
+        // Todo: Fix
+        public GoogleMapsAPIService(HttpClient httpClient, GoogleMapConfig config)
+        {
+            _client = httpClient;
+            _appId = config.AppId;
+            _appKey = config.AppKey;
+        }
+
+        // Todo: Fix
+        public async Task<IEnumerable<Parsed>> QueryLocations(string queryString)
+        {
+            string url = "https://www.google.com/maps/embed/v1/search?q="+ queryString + "&key=" + _appKey;
+            //API Key: 
+            //   <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA3ZXQrC6k4_x2gZ0x1wBtWCmm0BCP7dPs&callback=initMap"></script>
+            var response = await _client.GetStringAsync(url);
+            Root myDeserializedClass = JsonSerializer.Deserialize<Root>(response);
+
+            return myDeserializedClass.parsed;
+        }
+
+    }
