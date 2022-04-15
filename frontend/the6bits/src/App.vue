3c1a6ce81491b108957744d836074a6fa8511ebe
dantesecundino <73675658+dantesecundino@users.noreply.github.com>
2022-04-14 18:53:24 -0700

    viewReminder draft

--- frontend/the6bits/src/App.vue
+++ frontend/the6bits/src/App.vue
@@ -10,11 +10,17 @@
     <router-link :to="{ name: 'Registration' }"> Registration</router-link> |
     <router-link :to="{ name: 'MedSearch' }"> MedSearch</router-link>|
     <router-link :to="{ name: 'HotTopics' }"> HotTopics</router-link>|
-    <router-link :to="{name: 'getTLogs'}"> TrackingLog</router-link>
+    <router-link :to="{name: 'SearchFood'}"> SearchFood</router-link>|
+    <router-link :to="{name: 'HealthRecorder'}"> HealthRecorder</router-link>|
+    <router-link :to="{name: 'FavoriteDrugListPost'}"> Favorite Drugs List </router-link>| 
+    <router-link :to="{name: 'getTLogs'}"> TrackingLog</router-link>|
+    <router-link :to="{name: 'DietRecommendation'}"> Diet Recommendation</router-link>|
+    <router-link :to="{name: 'DeleteAccount'}"> Delete </router-link>|
+    <router-link :to="{name: 'ViewReminder'}"> ViewReminder </router-link>
 
 
   </nav>
-  <router-view />
+    <router-view />
 </template>
 
 <script>
