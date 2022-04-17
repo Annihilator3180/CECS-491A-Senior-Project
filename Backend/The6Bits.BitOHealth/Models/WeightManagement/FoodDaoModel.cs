436b489eed988e87cbdbfb2c65baae0c7b32f53c
RamiIsk <ramysaleh60@gmail.com>
2022-04-17 04:53:14 -0700

    merge

---
+++ Backend/The6Bits.BitOHealth/Models/WeightManagement/FoodDaoModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The6Bits.BitOHealth.Models
+{
+
+
+    public class FoodDaoModel
+    {
+
+
+
+
+
+
+
+        public string Username { get; set; }
+        public string FoodName { get; set; }
+        public string Description { get; set; }
+        public float Calories { get; set; }
+
+        public DateTime FoodLogDate { get; set; }
+        public float? Carbs { get; set; }
+        public float? Protein { get; set; }
+        public float? Fat { get; set; }
+
+        public FoodDaoModel(string username, string foodName, string description, float calories, DateTime foodLogDate, float? carbs = null, float? protein = null, float? fat = null)
+        {
+            Username = username;
+            FoodName = foodName;
+            Description = description;
+            Calories = calories;
+            FoodLogDate = foodLogDate;
+            Carbs = carbs;
+            Protein = protein;
+            Fat = fat;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ frontend/the6bits/src/components/WeightManagement/WMComponents/FoodLogRow.vue
@@ -0,0 +1,38 @@
+<template>
+    <div >
+        <li v-for="FoodItem in FoodLogs" :key="FoodItem">
+            {{ FoodItem.foodName }} | {{ FoodItem.calories }} | {{ FoodItem.description }} | {{ FoodItem.foodLogDate }}
+        </li>
+    </div> 
+</template>
+//TODO: ADD Dropdown and Delete Button
+<script>
+    import { GetAllFoodLogs }  from '@/components/WeightManagement/WeightManagement'
+    export default {
+        name : 'GetFoodLogs',
+        data() {
+        return {
+            FoodLogs : [],
+        }
+    },
+    methods:{
+        GetAllFoodLogs,
+        LoadGoal(){
+            //IS THIS TRASH?
+            this.GetAllFoodLogs().then(value => {
+                this.FoodLogs = value;
+                console.log(value);
+                })
+
+        },
+    },
+
+    beforeMount(){
+        this.LoadGoal();
+    },
+
+        
+    }
+
+
+</script>
