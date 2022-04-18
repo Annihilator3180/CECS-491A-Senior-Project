40c275540cf8336a14fb1e837430f5d7c35f3e95
sebastian <44591891+cbxss@users.noreply.github.com>
2022-04-17 17:33:47 -0700

    publish fix

--- frontend/the6bits/src/App.vue
+++ frontend/the6bits/src/App.vue
@@ -18,7 +18,6 @@
    <router-link :to="{name: 'FavoriteDrugListPost'}"> Favorite Drugs List </router-link> |
     <router-link :to="{name: 'DietRecommendation'}"> Diet Recommendation</router-link>|
     <router-link :to="{name: 'DeleteAccount'}"> Delete </router-link>|
-    <router-link :to="{name: 'ViewReminder'}"> ViewReminder </router-link>
   </nav>
     <router-view />
 </template>
