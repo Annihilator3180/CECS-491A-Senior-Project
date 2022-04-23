f9e4511b8bc7280e357b208c8c6f95e1dbfd8b20
emilysahyoun <emilysahyoun29@gmail.com>
2022-04-22 21:20:03 -0700

    Updates

--- frontend/the6bits/src/components/DietRecommendation.vue
+++ frontend/the6bits/src/components/DietRecommendation.vue
@@ -2,6 +2,40 @@
     <div class="form">
         <form>
             <div>
+                <p> Select a Timing: </p>
+                <select id="MealType" v-model="filters.mealType" @change="filterResults()">
+                    <option value=""> All </option>
+                    <option value="breakfast"> Breakfast </option>
+                    <option value="lunch"> Lunch </option>
+                    <option value="dinner"> Dinner </option>
+                    <option value="snack"> Snack </option>
+                    <option value="teatime"> Teatime </option>
+                </select>
+            </div>
+            <div>
+                <p> Select a Meal Type: </p>
+                <select id="Health" v-model="filters.health" @change="filterResults()">
+                    <option value=""> All </option>
+                    <option value="alcohol-free"> alcohol-free  </option>
+                    <option value="dairy-free"> dairy-free  </option>
+                    <option value="egg-free"> egg-free  </option>
+                    <option value="fish-free"> fish-free  </option>
+                    <option value="gluten-free"> gluten-free  </option>
+                    <option value="keto-friendly"> keto-friendly  </option>
+                    <option value="kosher"> kosher  </option>
+                    <option value="low-sugar"> low-sugar  </option>
+                    <option value="paleo"> paleo  </option>
+                    <option value="peanut-free"> peanut-free  </option>
+                    <option value="pescatarian"> pescatarian  </option>
+                    <option value="pork-free"> pork-free  </option>
+                    <option value="red-meat-free"> red-meat-free  </option>
+                    <option value="shellfish-free"> shellfish-free  </option>
+                    <option value="sugar-conscious"> sugar-conscious  </option>
+                    <option value="vegan"> vegan  </option>
+                    <option value="vegetarian"> vegetarian  </option>
+                </select>
+            </div>
+            <div>
                 <p>Answer the following questions for recipes</p>
             </div>
             <div>
@@ -49,62 +83,10 @@
 
                 <input type="text" id="excluded" v-model="formData.excluded" />
             </div>
-
-
-            <button @click="DietRecommendation" type="button">Search</button>
             <div>
-                <p> Select a Timing: </p>
-                <select id="MealType" v-model="filters.mealType" @change="filterResults()">
-                    <option value=""> All </option>
-                    <option value="breakfast"> Breakfast </option>
-                    <option value="lunch"> Lunch </option>
-                    <option value="dinner"> Dinner </option>
-                    <option value="snack"> Snack </option>
-                    <option value="teatime"> Teatime </option>
-                </select>
+                <button @click="DietRecommendation" type="button">Search</button>
             </div>
-            <div>
-                <p> Select a Meal Type: </p>
-                <select id="Health" v-model="filters.health" @change="filterResults()">
-                    <option value=""> All </option>
-                    <option value="alcohol-cocktail"> alcohol-cocktail  </option>
-                    <option value="alcohol-free"> alcohol-free  </option>
-                    <option value="celery-free"> celery-free  </option>
-                    <option value="crustacean-free"> crustacean-free  </option>
-                    <option value="dairy-free"> dairy-free  </option>
-                    <option value="DASH"> DASH  </option>
-                    <option value="egg-free"> egg-free  </option>
-                    <option value="fish-free"> fish-free  </option>
-                    <option value="fodmap-free"> fodmap-free  </option>
-                    <option value="gluten-free"> gluten-free  </option>
-                    <option value="immuno-supportive"> immuno-supportive  </option>
-                    <option value="keto-friendly"> keto-friendly  </option>
-                    <option value="kidney-friendly"> kidney-friendly  </option>
-                    <option value="kosher"> kosher  </option>
-                    <option value="low-fat-abs"> low-fat-abs  </option>
-                    <option value="low-potassium"> low-potassium  </option>
-                    <option value="low-sugar"> low-sugar  </option>
-                    <option value="lupine-free"> lupine-free  </option>
-                    <option value="Mediterranean"> Mediterranean  </option>
-                    <option value="mollusk-free"> mollusk-free  </option>
-                    <option value="mustard-free"> mustard-free  </option>
-                    <option value="no-oil-added"> no-oil-added  </option>
-                    <option value="paleo"> paleo  </option>
-                    <option value="peanut-free"> peanut-free  </option>
-                    <option value="pescatarian"> pescatarian  </option>
-                    <option value="pork-free"> pork-free  </option>
-                    <option value="red-meat-free"> red-meat-free  </option>
-                    <option value="sesame-free"> sesame-free  </option>
-                    <option value="shellfish-free"> shellfish-free  </option>
-                    <option value="soy-free"> soy-free  </option>
-                    <option value="sugar-conscious"> sugar-conscious  </option>
-                    <option value="sulfite-free"> sulfite-free  </option>
-                    <option value="tree-nut-free"> tree-nut-free  </option>
-                    <option value="vegan"> vegan  </option>
-                    <option value="vegetarian"> vegetarian  </option>
-                    <option value="wheat-free"> wheat-free  </option>
-                </select>
-            </div>
+
             <div id="DietRecommendation">
                 <template v-for="(values,index) in loadedRecepies" :key="index">
                     <div class="DietRecommendation">
