b701d7f8da79be4f4491be8b699a33e55ee59a3c
sebastian <44591891+cbxss@users.noreply.github.com>
2022-05-14 03:22:49 -0700

    fix tracker log

--- Backend/The6Bits.BitOHealth/Tests/The6Bits.BitOHealth.ServiceLayer.Test/ReminderServiceShould.cs
+++ Backend/The6Bits.BitOHealth/Tests/The6Bits.BitOHealth.ServiceLayer.Test/ReminderServiceShould.cs
@@ -37,7 +37,7 @@ namespace The6Bits.BitOHealth.ServiceLayer.Test
         public async void editReminder()
         {
             string name = "HW", description = "327 hw", date = "03-16-2022", time = "11:59 pm", repeat = "weekly";
-            await _reminderService.CreateReminder(username, name, description, date, time, repeat);
+            await _reminderService.CreateReminder(0, username, name, description, date, time, repeat);
             string holder = await _dao.EditReminder(username, "1", name, description, date, time, repeat);
             Assert.Equal("Reminder Edited", holder);
             await _dao.DeleteReminder(username, "1");
@@ -49,7 +49,7 @@ namespace The6Bits.BitOHealth.ServiceLayer.Test
         {
             string name = "HW", description = "Do 342 hw", date = "03-16-2022", time = "04:00 pm", repeat = "weekly";
             Thread.Sleep(5000);
-            string should = await _reminderService.CreateReminder(username, name, description, date, time, repeat);
+            string should = await _reminderService.CreateReminder(0,username, name, description, date, time, repeat);
             Assert.Equal("Reminder Created", should);
             await _reminderService.DeleteReminder(username, "1");
         }
@@ -58,7 +58,7 @@ namespace The6Bits.BitOHealth.ServiceLayer.Test
         public async void deleteReminder()
         {
             string name = "HW", description = "Do 342 hw", date = "03-16-2022", time = "04:00 pm", repeat = "weekly";
-            await _reminderService.CreateReminder(username, name, description, date, time, repeat);
+            await _reminderService.CreateReminder(0,username, name, description, date, time, repeat);
             string holder = await _dao.DeleteReminder(username, "1");
             Assert.Equal("Reminder Deleted", holder);
         }
